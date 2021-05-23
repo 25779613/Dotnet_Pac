3cff323dca8a02d22ad5249e4bfbd8e3c9a778e4
25779613 <duk.sal01@gmail.com>
2021-05-23 23:07:48 +0200

    student model added

---
+++ Dotnet_Prac/Models/StudentModel.cs
@@ -0,0 +1,17 @@
+namespace Dotnet_Prac.Models
+{
+    public class StudentModel
+    {
+        public int studentID{get;set;}
+        public string studentName{get;set;}
+
+        public string studentEmail{get;set;}
+
+        public string studentNumber{get;set;}
+
+        public string subject{get;set;}
+
+        public string studentDetails{get;set;}
+
+    }
+}
--- Dotnet_Prac/Startup.cs
+++ Dotnet_Prac/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,9 @@ namespace Dotnet_Prac
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<MySqlConnection>(_ => new MySqlConnection(Configuration["ConnectionStrings:Default"]));
+           // services.AddDbContext<StudentContext>(options => options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
